e4f68653d3d8c10a90aa16ab6bf90796fa1a549c
Luke <gdjiegz@gmail.com>
2020-10-07 08:23:03 +0800

    feat: 去掉一些没有太大意义的log

--- cloud-provider-payment/src/main/java/com/demo/payment/controller/PaymentController.java
+++ cloud-provider-payment/src/main/java/com/demo/payment/controller/PaymentController.java
@@ -1,13 +1,11 @@
 package com.demo.payment.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Slf4j
 @RefreshScope
 @RestController
 public class PaymentController {
@@ -17,7 +15,6 @@ public class PaymentController {
 
     @GetMapping("/v1/test")
     public String test() {
-        log.info("test " + info);
         return "test ===>" + info;
     }
 }
