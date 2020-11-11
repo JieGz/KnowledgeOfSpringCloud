a81007868386e72ecc1faf1220f7479051cc08f9
Luke <gdjiegz@gmail.com>
2020-11-11 09:11:03 +0800

    docs:考虑引入xxl-job(大众点评)替换elastic-job(当当网)

--- knowledge-elastic-job/src/main/java/com/demo/elastic/job/ElasticJobApplication.java
+++ knowledge-elastic-job/src/main/java/com/demo/elastic/job/ElasticJobApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * todo elastic job这个框架目前正在apache孵化中,目前2.1.5版本对zookeeper curator版本依赖过旧,可以考虑xxl-job
+ */
 @EnableElasticJob
 @SpringBootApplication
 public class ElasticJobApplication {
