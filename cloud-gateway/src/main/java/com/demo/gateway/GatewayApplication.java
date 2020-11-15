3b4086fb2199ca8c01d7b9d5182ea1ad1e94396f
Luke <gdjiegz@gmail.com>
2020-11-15 20:42:42 +0800

    docs:制定下一个要做的功能

--- cloud-gateway/src/main/java/com/demo/gateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/demo/gateway/GatewayApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+// TODO: 2020/11/15 封装一个好用的网络响应的组件
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
 }
